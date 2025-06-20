5f5f4ba53c032ff8ce8d106a2f20384226b7d9d8
Jeni-29 <jenifer.silva@grupointegrado.br>
2025-06-20 15:25:12 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,44 @@
+  //===================Mostra cada categoria ======================  
+    document.addEventListener("DOMContentLoaded", function () {
+    const botoesFiltro = document.querySelectorAll(".filtro-btn");
+    const grupos = document.querySelectorAll(".grupo");
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const filtroInicial = urlParams.get('categoria');
+
+    if (filtroInicial) {
+        filtrarGrupos(filtroInicial);
+    }
+
+    botoesFiltro.forEach(botao => {
+        botao.addEventListener("click", () => {
+            const filtro = botao.getAttribute("data-filtro");
+            filtrarGrupos(filtro);
+        });
+    });
+
+// ================= parte para puxar as seção na pagina home================
+
+    function filtrarGrupos(filtro) {
+        grupos.forEach(grupo => {
+            // Busca todos os produtos dentro da seção
+            const produtos = grupo.querySelectorAll(".produto-card");
+            let algumVisivel = false;
+
+            produtos.forEach(produto => {
+                const categoria = produto.getAttribute("data-categoria");
+
+                // Verifica se o produto é da categoria clicada
+                if (filtro === "Todos" || categoria === filtro) {
+                    produto.style.display = "block";
+                    algumVisivel = true;
+                } else {
+                    produto.style.display = "none";
+                }
+            });
+
+            // Só mostra a seção se algum produto dela estiver visível
+            grupo.style.display = algumVisivel ? "block" : "none";
+        });
+    }
+});
